b4c07ba76e3649c0c051ae1e7a72d26476419382
Karimsameh100 <karimsameh807@gmail.com>
2024-08-30 17:35:36 +0300

    update main

--- crowdfounding/founding/urls.py
+++ crowdfounding/founding/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('projectlist/', views.project_list, name='project_list'),
     path('project/<int:pk>/', views.project_detail, name='project_detail'),
     path('create/', views.create_project, name='create_project'),
