63ffe9c8d89b10944bd7c6bf612a5d3decc38425
“3EnasMelad” <https://github.com/3EnasMelad>
2024-09-06 01:14:19 +0300

    update the validation of all fields,using auth_user instead of (my user)-links2

--- crowdfounding/founding/models.py
+++ crowdfounding/founding/models.py
@@ -55,3 +55,17 @@ class Rating(models.Model):
 
 
 
+# ----------------------activation
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
+from datetime import timedelta
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=False)
+    activation_token = models.CharField(max_length=64, null=True, blank=True)
+    activation_token_created_at = models.DateTimeField(default=now)
+
+    def is_token_expired(self):
+        return self.activation_token_created_at < now() - timedelta(hours=24)
