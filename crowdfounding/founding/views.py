ee8c2d26b60b04271980bb2f79f2a93dd2287f13
“3EnasMelad” <https://github.com/3EnasMelad>
2024-09-05 16:54:41 +0300

    update the validation of all fields,using auth_user instead of (my user)2

--- crowdfounding/founding/views.py
+++ crowdfounding/founding/views.py
@@ -87,24 +87,6 @@ from .models import MyUser
 
 
 
-# def register(request):
-#     if request.method == 'POST':
-#         form = RegistrationForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             user = MyUser.objects.create(
-#                 first_name=form.cleaned_data['first_name'],
-#                 last_name=form.cleaned_data['last_name'],
-#                 email=form.cleaned_data['email'],
-#                 password=form.cleaned_data['password'],  
-#                 mobile_phone=form.cleaned_data['mobile_phone'],
-#                 profile_picture=form.cleaned_data['profile_picture'],
-#             )
-#             return redirect('login')
-#     else:
-#         form = RegistrationForm()
-
-#     return render(request, 'founding/registerform.html', {'form': form})
-
 
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
